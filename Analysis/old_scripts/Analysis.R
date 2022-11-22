d3acdfbb35bc70134abb5944bbcbedc5a4c62c05
gmchen <gregorymchen@gmail.com>
2022-11-22 15:49:51 -0500

    Cleaned up file paths

--- Analysis/old_scripts/Analysis.R
+++ Analysis/old_scripts/Analysis.R
@@ -1,18 +1,16 @@
-
-
 library(bigchess)
 library(chess)
 library(ggrepel)
 
-data_import <- read.csv("~/Box/repos/Lichess_Trap_Finder/lichess-api-queries/data_from_queries/position_data_white_attacking.txt", sep="\t")
-data_import <- read.csv("~/Box/repos/Lichess_Trap_Finder/lichess-api-queries/data_from_queries/position_data_black_attacking.txt", sep="\t")
-data_import <- read.csv("~/Box/repos/Lichess_Trap_Finder/lichess-api-queries/data_from_queries/position_data_white_attacking_merged.txt", sep="\t")
-data_import <- read.csv("~/Box/repos/Lichess_Trap_Finder/lichess-api-queries/data_from_queries/old_position_data_white_attacking.txt", sep="\t")
-data_import <- read.csv("~/Box/repos/Lichess_Trap_Finder/lichess-api-queries/data_from_queries/run_to_above_32000_position_data_white_attacking.txt", sep="\t")
-data_import <- read.csv("~/Box/repos/Lichess_Trap_Finder/Analysis/processed_data/filtered_data_san_fen_cluster.tsv", sep="\t")
-data_import <- read.csv("~/Box/repos/Lichess_Trap_Finder/Analysis/processed_data/black_filtered_data_san_fen_cluster.tsv", sep="\t")
-cluster_representatives_without_analysis <- read.csv("~/Box/repos/Lichess_Trap_Finder/Analysis/processed_data/white_cluster_representatives_without_analysis.tsv", sep="\t")
-cluster_representatives_with_analysis <- read.csv("~/Box/repos/Lichess_Trap_Finder/Analysis/processed_data/white_cluster_representatives_with_analysis.tsv", sep="\t")
+data_import <- read.csv("../../lichess-api-queries/data_from_queries/position_data_white_attacking.txt", sep="\t")
+data_import_2 <- read.csv("../../lichess-api-queries/data_from_queries/position_data_black_attacking.txt", sep="\t")
+data_import <- read.csv("../../lichess-api-queries/data_from_queries/position_data_white_attacking_merged.txt", sep="\t")
+data_import <- read.csv("../../lichess-api-queries/data_from_queries/old_position_data_white_attacking.txt", sep="\t")
+data_import <- read.csv("../../lichess-api-queries/data_from_queries/run_to_above_32000_position_data_white_attacking.txt", sep="\t")
+data_import <- read.csv("../../Analysis/processed_data/filtered_data_san_fen_cluster.tsv", sep="\t")
+data_import <- read.csv("../../Analysis/processed_data/black_filtered_data_san_fen_cluster.tsv", sep="\t")
+cluster_representatives_without_analysis <- read.csv("../../Analysis/processed_data/white_cluster_representatives_without_analysis.tsv", sep="\t")
+cluster_representatives_with_analysis <- read.csv("../../Analysis/processed_data/white_cluster_representatives_with_analysis.tsv", sep="\t")
 
 filtered_data <- data_import
 
@@ -20,7 +18,7 @@ filtered_data <- data_import
 filtered_data <- data_import[data_import$prob_trimmed > 0.1 & data_import$prob > 0.01 & data_import$white_win_prob > 0.55 & data_import$white_wins + data_import$draws + data_import$black_wins > 500,]
 
 # For black
-filtered_data <- data_import[data_import$prob_trimmed > 0.1 & data_import$prob > 0.01 & data_import$black_win_prob > 0.55 & data_import$white_wins + data_import$draws + data_import$black_wins > 500,]
+filtered_data_2 <- data_import[data_import$prob_trimmed > 0.1 & data_import$prob > 0.01 & data_import$black_win_prob > 0.55 & data_import$white_wins + data_import$draws + data_import$black_wins > 500,]
 
 # For white
 filtered_data <- filtered_data[rev(order(filtered_data$white_win_prob)),]
@@ -115,4 +113,4 @@ cluster_representatives_without_analysis$cluster_idx <- 1:nrow(cluster_represent
 out <- merge(cluster_representatives_without_analysis, cluster_representatives_with_analysis[,c("san", "analysis_url")], by="san", all.x = TRUE)
 out <- out[,colnames(cluster_representatives_with_analysis)]
 out[out$move_index %% 2 == 1,]$stockfish_eval <- - out[out$move_index %% 2 == 1,]$stockfish_eval
-write.table(out, file="~/Box/repos/Lichess_Trap_Finder/Analysis/processed_data/white_cluster_representatives_with_analysis.tsv", sep="\t", row.names = FALSE, quote=FALSE)
+write.table(out, file="../../Analysis/processed_data/white_cluster_representatives_with_analysis.tsv", sep="\t", row.names = FALSE, quote=FALSE)
